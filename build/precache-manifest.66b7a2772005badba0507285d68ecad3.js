532ec246fe02b00208434b376ec05b108e30ba5a
Kuntesh Patel <khpatel@gmail.com>
2020-06-18 13:56:38 -0300

    add build

---
+++ build/precache-manifest.66b7a2772005badba0507285d68ecad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51cb87f8cc69f858fd39c5916012e31d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bade5fc57f3a5f3d3f45",
+    "url": "/static/css/main.71c630e9.chunk.css"
+  },
+  {
+    "revision": "d7bf016be257a5efd661",
+    "url": "/static/js/2.b4486250.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b4486250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bade5fc57f3a5f3d3f45",
+    "url": "/static/js/main.1988b5c4.chunk.js"
+  },
+  {
+    "revision": "6697ec51558987e1c37c",
+    "url": "/static/js/runtime-main.89b6277a.js"
+  }
+]);
